0909591db02e1fd88422b1cbf94f3e2973aa1aec
batista29 <natabatista@gmail.com>
2024-09-30 15:33:17 -0300

    Iniciando backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,30 @@
-import http from "http";
+import express from 'express';
+import { Router, Request, Response } from 'express'
 
+const backend = express();
+const route = Router();
 const port = 3000;
 
-const server = http.createServer((request, response)=>{
-    // vou criar as rotas do meu servidor aqui.
-    response.writeHead(200,{'Content-Type': 'text/html'});
-    response.end('<h1>It works!</h1>');
+backend.use(express.json());
+
+//rotas de serviços
+backend.post('/login', (req: Request, res: Response) => {
+    res.send('Login funcionando');
+})
+
+backend.post('/signUp', (req, res) => {
+    res.send('Cadastro funcionando');
 });
 
-server.listen(port,()=>{
-    console.log(`Servidor rodando na porta ${port}`);
+backend.get('/carteira', (req: Request, res: Response) => {
+    res.send('Carteira funcionando');
+})
+
+backend.get('/apostasFeitas', (req, res) => {
+    res.send('Apostas feitas funcionando');
 });
+
+//Subindo servidor
+backend.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`);
+})
