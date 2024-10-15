953e9f9a60cab654cbb067042292883ca97e502a
batista29 <natabatista@gmail.com>
2024-10-15 11:02:32 -0300

    Conectando com banco mysql, função: carteira

--- backend/src/accounts/accounts.ts
+++ backend/src/accounts/accounts.ts
@@ -1,13 +1,23 @@
 import { Request, RequestHandler, Response } from "express";
+import { RowDataPacket, FieldPacket } from 'mysql2';
 
 /*
     Nampespace que contém tudo sobre "contas de usuários"
 */
 export namespace AccountsHandler {
 
-    /**
-     * Tipo UserAccount
-     */
+    function connectDatabase() {
+        var mysql = require('mysql2');
+        const conn = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'teste'
+        });
+
+        return conn;
+    }
+
     export type UserAccount = {
         name: string;
         email: string;
@@ -92,38 +102,45 @@ export namespace AccountsHandler {
         }
     }
 
-    //Adicionando deposito
-    function addValue(newDeposit: UserWallet): number {
-        walletDatabase.push(newDeposit);
-        return walletDatabase.length;
-    }
-
     //Verificando se os dados estão vindo e chamando a função de add deposito
     export const deposit: RequestHandler = (req: Request, res: Response) => {
         const id = Number(req.get('id'));
         const value = Number(req.get('value'));
 
+        //verificando se nao vem campo vazio
         if (id && value) {
-            const newDeposit: UserWallet = {
-                id: id,
-                value: value
-            }
-            const ok = addValue(newDeposit);
-            res.statusCode = 200;
-            res.send(`Quantidade de depositos feitos: ${ok}`);
+            //conectando com o banco, fiz a função para nao ter que copiar 7 linhas toda hora
+            let conn = connectDatabase();
+            conn.query(`INSERT INTO carteira VALUES(${id}, ${value});`, function (err: Error, data: RowDataPacket[], fields: FieldPacket) {
+                if (!err) {
+                    res.statusCode = 200;
+                    //retornando 1, que é a quantidade de campos alterados, se nao retornar 1 é porque deu erro
+                    res.send(fields);
+                } else {
+                    //erro caso nao envie a informações corretas para o banco
+                    res.statusCode = 400;
+                    res.send("Error")
+                }
+            });
         } else {
+            //erro caso nao tenha todos os campos preenchidos
             res.statusCode = 400;
+            res.send("Error");
         }
     }
 
-    //Para ver os depositos
+    //Para ver os depositos feitos
     export const seeDeposits: RequestHandler = (req: Request, res: Response) => {
-        console.log(walletDatabase.length);
-        if (walletDatabase.length >= 1) {
-            res.statusCode = 200;
-            res.json(walletDatabase);
-        } else {
-            res.statusCode = 404;
-        }
+        let conn = connectDatabase();
+
+        conn.query('SELECT * FROM carteira;', function (err: Error, data: RowDataPacket[]) {
+            if (!err) {
+                res.statusCode = 200;
+                res.send(data);
+            } else {
+                res.statusCode = 400;
+                res.send("Error")
+            }
+        });
     }
 }
---
+++ bcd/teste.sql
@@ -0,0 +1,8 @@
+drop database teste;
+create database teste;
+use teste;
+
+create table carteira(
+    id INTEGER NOT NULL,
+    value INTEGER NOT NULL
+);
