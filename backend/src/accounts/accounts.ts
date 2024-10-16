86160b6d65960ca73664c0713d7983a141a53893
MARIA GABRIELLA XAVIER PUCCINELLI <maria.gxp@puccampinas.edu.br>
2024-10-16 10:16:52 -0300

    wallet criado

--- backend/src/accounts/accounts.ts
+++ backend/src/accounts/accounts.ts
@@ -25,18 +25,7 @@ export namespace AccountsHandler {
         birthdate: string;
     };
 
-    //Carteira do usuario
-    export type UserWallet = {
-        id: number;
-        value: number;
-    }
-
-    // Array que representa uma coleção de contas. 
-    let accountsDatabase: UserAccount[] = [];
-
-    //array para armazenar valores de contas dos usuarios
-    let walletDatabase: UserWallet[] = [];
-
+   
     /**
      * Salva uma conta no banco de dados. 
      * @param ua conta de usuário do tipo @type {UserAccount}
@@ -101,46 +90,3 @@ export namespace AccountsHandler {
             res.send("Email ou senha incorretos");
         }
     }
-
-    //Verificando se os dados estão vindo e chamando a função de add deposito
-    export const deposit: RequestHandler = (req: Request, res: Response) => {
-        const id = Number(req.get('id'));
-        const value = Number(req.get('value'));
-
-        //verificando se nao vem campo vazio
-        if (id && value) {
-            //conectando com o banco, fiz a função para nao ter que copiar 7 linhas toda hora
-            let conn = connectDatabase();
-            conn.query(`INSERT INTO carteira VALUES(${id}, ${value});`, function (err: Error, data: RowDataPacket[], fields: FieldPacket) {
-                if (!err) {
-                    res.statusCode = 200;
-                    //retornando 1, que é a quantidade de campos alterados, se nao retornar 1 é porque deu erro
-                    res.send(fields);
-                } else {
-                    //erro caso nao envie a informações corretas para o banco
-                    res.statusCode = 400;
-                    res.send("Error")
-                }
-            });
-        } else {
-            //erro caso nao tenha todos os campos preenchidos
-            res.statusCode = 400;
-            res.send("Error");
-        }
-    }
-
-    //Para ver os depositos feitos
-    export const seeDeposits: RequestHandler = (req: Request, res: Response) => {
-        let conn = connectDatabase();
-
-        conn.query('SELECT * FROM carteira;', function (err: Error, data: RowDataPacket[]) {
-            if (!err) {
-                res.statusCode = 200;
-                res.send(data);
-            } else {
-                res.statusCode = 400;
-                res.send("Error")
-            }
-        });
-    }
-}
