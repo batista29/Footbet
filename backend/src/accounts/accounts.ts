f023d0aed4c2e3b7a8795838b0433d0d32201482
Ester Amaral <ester.teresa10@gmail.com>
2024-10-24 08:56:41 -0300

    AddnewEvent, DeleteEvent e FinishEvent ok

--- backend/src/accounts/accounts.ts
+++ backend/src/accounts/accounts.ts
@@ -1,93 +1,197 @@
 import { Request, RequestHandler, Response } from "express";
-import { RowDataPacket, FieldPacket } from 'mysql2';
+import oracledb from "oracledb";
 
-/*
-    Nampespace que contém tudo sobre "contas de usuários"
-*/
 export namespace AccountsHandler {
 
-    function connectDatabase() {
-        var mysql = require('mysql2');
-        const conn = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'teste'
-        });
+    oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-        return conn;
-    }
+// Função para estabelecer conexão com o OracleDB
+    async function getConnection() {
+        const connection = await oracledb.getConnection({
+            user: "hr",
+            password: "mypw",
+            connectString: "localhost/FREEPDB1"
+        });
+}
 
     export type UserAccount = {
-        name: string;
-        email: string;
+        user_id: string;
+        full_name: string;
+        username: string;
         password: string;
-        birthdate: string;
+        email: string;
+        birth_date: Date;
     };
 
-   
-    /**
-     * Salva uma conta no banco de dados. 
-     * @param ua conta de usuário do tipo @type {UserAccount}
-     * @returns @type { number } o código da conta cadastrada como posição no array.
-     */
-    export function saveNewAccount(ua: UserAccount): number {
-        accountsDatabase.push(ua);
-        return accountsDatabase.length;
-    }
-
-    /**
-     * Função para tratar a rota HTTP /signUp. 
-     * @param req Requisição http tratada pela classe @type { Request } do express
-     * @param res Resposta http a ser enviada para o cliente @type { Response }
-     */
-    export const createAccountRoute: RequestHandler = (req: Request, res: Response) => {
-        // Passo 1 - Receber os parametros para criar a conta
-        const pName = req.get('name');
-        const pEmail = req.get('email');
-        const pPassword = req.get('password');
-        const pBirthdate = req.get('birthdate');
-
-        if (pName && pEmail && pPassword && pBirthdate) {
-            // prosseguir com o cadastro... 
-            const newAccount: UserAccount = {
-                name: pName,
-                email: pEmail,
-                password: pPassword,
-                birthdate: pBirthdate
-            }
-            
-            const ID = saveNewAccount(newAccount);
-            res.statusCode = 200;
-            res.send(`Nova conta adicionada. Código: ${ID}`);
-        } else {
-            res.statusCode = 400;
-            res.send("Parâmetros inválidos ou faltantes.");
+    // Função para salvar uma nova conta
+    export async function saveNewAccount(newAccount: UserAccount): Promise<string | undefined> {
+        try {
+            const connection = await getConnection();
+            const result = await connection.execute(
+                `INSERT INTO Users (user_id, full_name, username, password, email, birth_date)
+                VALUES (:user_id, :full_name, :username, :password, :email, :birth_date)`,
+                {
+                    user_id: newAccount.user_id,
+                    full_name: newAccount.full_name,
+                    username: newAccount.username,
+                    password: newAccount.password,
+                    email: newAccount.email,
+                    birth_date: newAccount.birth_date
+                },
+                { autoCommit: true }
+            );
+            await connection.close();
+            return result.lastRowid;
+        } 
+        catch (err) {
+            console.error("Erro ao salvar a conta!", err);
+            return undefined;
         }
     }
 
-    //Para ver se a conta existe e as informações estão corretas
-    function verifyAccount(email: string, password: string): boolean {
-        let exists: boolean = false;
+    // Função para verificar se a conta existe e as informações estão corretas
+    async function verifyAccount(email: string, password: string): Promise<boolean> {
+        try {
+            const connection = await getConnection();
+            const result = await connection.execute(
+                `SELECT COUNT(*) AS count
+                FROM Users
+                WHERE email = :email AND password = :password`,
+                { email, password }
+            );
 
-        accountsDatabase.find(account => {
-            if (email === account.email && password === account.password) {
-                exists = true;
-            }
-        })
-
-        return exists;
+            await connection.close();
+            const count = (result.rows as any[])[0]?.count || 0;
+            return count > 0;
+        } catch (err) {
+            console.error("Erro ao verificar conta! ", err);
+            return false;
+        }
     }
 
-    export const login: RequestHandler = (req: Request, res: Response) => {
+    //funcao para tratar o cadastro
+    export const createAccountRoute: RequestHandler = async(req: Request, res: Response) => {
+        // Passo 1 - Receber os parâmetros para criar a conta
+        const userId = req.get('userid');
+        const fullName = req.get('fullname');
+        const username = req.get('username');
+        const password = req.get('password');
+        const email = req.get('email');
+        const birthDate = req.get('birth date');
+
+        if (userId && fullName && username && password && email && birthDate) {
+            // prosseguir com o cadastro...
+            const newAccount: UserAccount = {
+                user_id: userId,
+                full_name: fullName,
+                username: username,
+                password: password,
+                email: email,
+                birth_date: new Date(birthDate)
+            };
+            const ID = await saveNewAccount(newAccount);
+
+            if (ID) {
+                res.status(200).send(`Nova conta adicionada. Código: ${ID}`);
+            } 
+            else {
+                res.status(500).send("Erro ao criar a conta. Tente novamente.");
+            }
+            }
+            else {
+                res.status(400).send("Parâmetros inválidos ou faltantes.");
+            }
+        };
+
+
+    //funcao para tratar o login
+    export const login: RequestHandler = async(req: Request, res: Response) => {
+        
         const email = req.get('email');
         const password = req.get('password');
 
-        if (email && password && verifyAccount(email, password)) {
-            res.statusCode = 200;
-            res.send(`Login efetuado`);
+        if (email && password) {
+            const isValid = await verifyAccount(email, password);
+                if (isValid) {
+                    res.status(200).send("Login efetuado com sucesso.");
+                } else {
+                    res.status(401).send("Email ou senha incorretos.");
+                }
+            } else {
+                res.status(400).send("Parâmetros inválidos ou faltantes.");
+            }
+        };
+
+    //Função de atualização de conta
+    export const updateAccount: RequestHandler = async (req: Request, res: Response) => {
+        const userId = req.get('userid');
+        const fullName = req.get('fullname');
+        const username = req.get('username');
+        const password = req.get('password');
+        const email = req.get('email');
+        const birthDate = req.get('birth date');
+
+        if (userId && (fullName || username || password || email || birthDate)) {
+            try {
+                const connection = await getConnection();
+                const result = await connection.execute(
+                    `UPDATE Users SET 
+                        full_name = COALESCE(:full_name, full_name),
+                        username = COALESCE(:username, username),
+                        password = COALESCE(:password, password),
+                        email = COALESCE(:email, email),
+                        birth_date = COALESCE(:birth_date, birth_date)
+                    WHERE user_id = :user_id`,
+                    {
+                        user_id: userId,
+                        full_name: fullName,
+                        username: username,
+                        password: password,
+                        email: email,
+                        birth_date: birthDate ? new Date(birthDate) : undefined
+                    },
+                    { autoCommit: true }
+                );
+
+                await connection.close();
+                if (result.rowsAffected) {
+                    res.status(200).send(`Conta com ID ${userId} atualizada com sucesso.`);
+                } else {
+                    res.status(404).send(`Usuário com ID ${userId} não encontrado.`);
+                }
+            } catch (error) {
+                console.error("Error updating account:", error);
+                res.status(500).send("Erro ao atualizar a conta.");
+            }
         } else {
-            res.statusCode = 400;
-            res.send("Email ou senha incorretos");
+            res.status(400).send("Parâmetros inválidos ou faltantes.");
         }
-    }
+    };
+
+    //Função de deletar a conta
+    export const deleteAccount: RequestHandler = async (req: Request, res: Response) => {
+        const userId = req.get('userid');
+        if (userId) {
+            try {
+                const connection = await getConnection();
+                const result = await connection.execute(
+                    `DELETE FROM User WHERE user_id = user_id`,
+                    {user_id: userId},
+                    {autoCommit: true}
+                );
+
+                await connection.close();
+                if (result.rowsAffected) {
+                    res.status(200).send(`Conta com ID ${userId} excluida com sucesso.`);
+                } else {
+                    res.status(404).send(`Usuário com ID ${userId} não encontrado.`);
+                }
+            } catch (error) {
+                console.error("Error deleting account:", error);
+                res.status(500).send("Erro ao atualizar a conta.");
+            }
+        } else {
+            res.status(400).send("Parâmetros 'userid' inválidos ou faltantes.");
+        }
+    };
+}
--- backend/src/events/events.ts
+++ backend/src/events/events.ts
@@ -1,37 +1,58 @@
 import {Request, RequestHandler, Response} from "express";
-
+import oracledb, {Connection} from "oracledb";
 // namespace para agrupar todos os metodos e funções relacionados aos eventos
 export namespace EventsHandler {
 
-    //Tipo Eventos
+    oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
+    //Conexão com bd
+        async function getConnection() {
+            const connection = await oracledb.getConnection({
+                user: "hr",
+                password: "mypw",
+                connectString: "localhost/FREEPDB1"
+            });
+    }
+
+    //Tipo Eventos
     type Event = {
         id: number;
         title: string;
         description: string;
         eventDate: Date;
+        betsStart: Date;
+        betsEnd: Date;
         status: string;
     };
 
-    //Array para simular bd
-    let eventsArray: Event[]=[
-        {id:1,title: "evento 1", description: "teste de eventos 1", eventDate: new Date('10/02/2005'), status:"accepted"},
-        {id:2,title: "evento 2", description: "teste de eventos 2", eventDate: new Date('30/12/2023'), status:"waiting"},
-        {id:3,title: "evento 3", description: "teste de eventos 3", eventDate: new Date('15/10/2024'), status: "rejected"},
-    ]
-
+    //Conexão com bd
+    
     //função para salvar evento - Alterar para salvar no bd -
-    export function saveNewEvent(ev: Event): number{
-        eventsArray.push(ev);
-        return eventsArray.length;
+    async function saveNewEvent(ev: Event): Promise<void>{
+        const connection = await getConnection();
+        await connection.execute(
+        `INSERT INTO Event (id, title, description, eventDate, betsStar, betsEnd, status)
+        VALUES (:id, :title, :description, :eventDate, :betsStar, :betsEnd, :status)`,
+        {
+            id: ev.id,
+            title: ev.title,
+            description: ev.description,
+            eventDate: ev.eventDate,
+            betsStart: ev.betsStart,
+            betsEnd: ev.betsEnd,
+            status: ev.status
+        },
+        { autoCommit: true });
     }
-
+    
     export const addNewEventRoute : RequestHandler = (req, resp)=>{
         // Receber os parametros para criar a conta 
         const pId = req.get('id');
         const pTitle = req.get('title');
         const pDescription = req.get('description');
-        const pEventDate = req.get('password');
+        const pEventDate = req.get('eventDate');
+        const pBetsStar = req.get('betsStar');
+        const pBetsEnd = req.get('betsEnd');
         const pStatus = "waiting";
 
         //Verificar se os campos não são vazios
@@ -41,7 +62,9 @@ export namespace EventsHandler {
                 title: pTitle,
                 description: pDescription,
                 eventDate: new Date(pEventDate),
-                status: pStatus,
+                betsStart: new Date(pBetsStar),
+                betsEnd: new Date(pBetsEnd),
+                status: pStatus
             };
             const cont = saveNewEvent(newEvent);
             resp.statusCode=  200;
@@ -52,14 +75,14 @@ export namespace EventsHandler {
         }
 
     }
-    //função para "remover" eventos - Alterar para remover no bd -
-    export function deleteEvent(id: number): void{
 
-        const resul= eventsArray.find(ev =>{
-            if(id === ev.id){
-                
-            }
-        })
+    //função para "remover" eventos - Alterar para remover no bd -
+    export async function deleteEvent(id: number): Promise<void>{
+        const connection = await getConnection();
+        const result = await connection.execute(
+        `Update Event set status = 'deleted' where id = :id`,
+        {id},
+        { autoCommit: true });
     }
     //rota para excluir evento
     export const deleteEventRoute : RequestHandler = (req, resp)=>{
@@ -68,7 +91,28 @@ export namespace EventsHandler {
 
         //Verificar se os campos não são vazios
         if(pId){
-            
+            deleteEvent(Number(pId));
+        }else{
+            resp.statusCode=  400;
+            resp.send(`Erro na obtenção dos dados ou dados inválidos`);
+        }
+    }
+    //função para "remover" eventos - Alterar para remover no bd -
+    export async function finishEvent(id: number): Promise<void>{
+        const connection = await getConnection();
+        const result = await connection.execute(
+        `Update Event set status = 'finished' where id = :id`,
+        {id},
+        { autoCommit: true });
+    }
+    //rota para excluir evento
+    export const finishEventRoute : RequestHandler = (req, resp)=>{
+        // Receber os parametros para criar a conta 
+        const pId = req.get('id');
+
+        //Verificar se os campos não são vazios
+        if(pId){
+            finishEvent(Number(pId));
         }else{
             resp.statusCode=  400;
             resp.send(`Erro na obtenção dos dados ou dados inválidos`);
