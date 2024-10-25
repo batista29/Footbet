2ab39354abe1939510a619118afb563fd975f8de
batista29 <natabatista2908@gmail.com>
2024-10-25 16:53:26 -0300

    Acessando o banco e vendo o saldo para o usuario nao tirar dinheiro que nao possui

--- backend/src/wallet/wallet.ts
+++ backend/src/wallet/wallet.ts
@@ -1,5 +1,6 @@
 import { Request, RequestHandler, Response } from "express";
 import { RowDataPacket, FieldPacket } from 'mysql2';
+import { resolve } from "path";
 
 export namespace walletHandler {
 
@@ -19,17 +20,6 @@ export namespace walletHandler {
         });
 
         return conn;
-    }
-
-    async function seeBalance(id_user: number) {
-        if (id_user) {
-            let conn = connectDatabase();
-            conn.query(`SELECT SUM(value) as 'saldo' FROM Transacao WHERE id_user = ${id_user};`, function (err: Error, data: RowDataPacket[]) {
-                if (!err) {
-                    return data[0].saldo;
-                }
-            });
-        }
     }
 
     //Verificando se os dados estão vindo e chamando a função de add deposito
@@ -62,21 +52,37 @@ export namespace walletHandler {
         }
     }
 
-    export const withdrawFunds: RequestHandler = (req: Request, res: Response) => {
+    async function seeBalance(id_user: number) {
+        return await new Promise((resolve, reject) => {
+            if (id_user) {
+                let conn = connectDatabase();
+                conn.query(`SELECT SUM(value) as 'saldo' FROM Transacao WHERE id_user = ${id_user};`, function (err: Error, data: RowDataPacket[]) {
+                    // console.log();
+                    if (!err || data && data.length > 0) {
+                        const carteira: UserWallet = {
+                            id: id_user,
+                            value: data[0].saldo as number
+                        }
+                        resolve(data[0].saldo);
+                    }
+                    reject(null);
+                });
+            }
+        })
+    }
+
+    export const withdrawFunds: RequestHandler = async (req: Request, res: Response) => {
         const id_wallet = Number(req.get('id_wallet'));
         const id_user = Number(req.get('id_user'));
-        let value = -1 * Number(req.get('value'));
+        let value = Number(req.get('value'));
         const type = 'saque';
-        const balance = seeBalance(id_user);
-
-        console.log("teste ", balance)
+        const balance = Number(await seeBalance(id_user));
 
         //verificando se nao vem campo vazio
-        if (id_wallet && id_user && value && type) {
-            console.log(value)
+        if (id_wallet && id_user && value && type && balance >= value) {
             //conectando com o banco, fiz a função para nao ter que copiar 7 linhas toda hora
             let conn = connectDatabase();
-            conn.query(`INSERT INTO Transacao VALUES(${id_wallet},${id_user}, ${value}, '${type}');`, function (err: Error, data: RowDataPacket[], fields: FieldPacket) {
+            conn.query(`INSERT INTO Transacao VALUES(${id_wallet},${id_user}, -${value}, '${type}');`, function (err: Error, data: RowDataPacket[], fields: FieldPacket) {
                 if (!err) {
                     res.statusCode = 200;
                     //retornando 1, que é a quantidade de campos alterados, se nao retornar 1 é porque deu erro
@@ -189,6 +195,7 @@ export namespace walletHandler {
 
 //             if (id_wallet && id_user && value && type) {
 //                 if (balance >= Math.abs(value)) {
+
 //                     let conn = connectDatabase();
 //                     conn.query(`INSERT INTO Transacao VALUES(${id_wallet},${id_user}, ${value}, '${type}');`, function (err: Error, data: RowDataPacket[], fields: FieldPacket) {
 //                         if (!err) {
