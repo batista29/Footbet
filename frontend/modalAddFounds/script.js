48a487273a29c62f0ef822ccda75395a5bb92d05
batista29 <natabatista2908@gmail.com>
2024-11-24 13:36:34 -0300

    Estrutura modal feita

---
+++ frontend/modalAddFounds/script.js
@@ -0,0 +1,9 @@
+function carregar() {
+    const modalFounds = document.getElementById('modalFounds');
+    modalFounds.classList.remove('model');
+}
+
+function closeModal() {
+    const modalFounds = document.getElementById('modalFounds');
+    modalFounds.classList.add('model');
+}
