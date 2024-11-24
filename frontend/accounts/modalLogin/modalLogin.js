ed7d3d8b23bf262509b317cab032908c80577d29
Maria Gabriella Xavier Puccinelli <mgabriella.puccinelli@gmail.com>
2024-11-24 16:34:56 -0300

    atualizacao accounts

---
+++ frontend/accounts/modalLogin/modalLogin.js
@@ -0,0 +1,10 @@
+function openModal() {
+    const modalFounds = document.getElementById('modalFounds');
+    modalFounds.classList.remove('model');
+}
+
+//Função para fechar o modal
+function closeModal() {
+    const modalFounds = document.getElementById('modalFounds');
+    modalFounds.classList.add('model');
+}
