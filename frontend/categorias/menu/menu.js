a102098eab7089f7a42c8256b7cf260941cf97e8
batista29 <natabatista2908@gmail.com>
2024-11-27 20:28:24 -0300

    Pesquisa funcionando por titulo, descricao e categoria

--- frontend/categorias/menu/menu.js
+++ frontend/categorias/menu/menu.js
@@ -1,3 +1,62 @@
+async function pesquisar() {
+  const inpPesquisa = document.getElementById('inpPesquisa').value;
+  console.log(inpPesquisa);
+
+  const myHeaders = new Headers();
+
+  myHeaders.append("keyword", `${inpPesquisa}`);
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    redirect: "follow"
+  };
+
+  try {
+    const response = await fetch("http://127.0.0.1:3000/searchEvents", requestOptions);
+
+    console.log(inpPesquisa);
+    let message = await response.text();
+    message = JSON.parse(message);
+
+    const eventosContainer = document.querySelector('.carousel'); // A div onde os cards ficarão
+
+    // Limpar conteúdos antigos, se necessário
+    eventosContainer.innerHTML = '';
+
+    message.forEach(e => {
+      const data = new Date(e.inicioApostas);
+
+      const dia = data.getDate().toString().padStart(2, '0');
+      const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+      const ano = data.getFullYear();
+
+      const dataBR = `${dia}/${mes}/${ano}`;
+
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      // Adicionar os dados ao card
+      card.innerHTML = `
+              <span class="data">${dataBR || 'Data indisponível'}</span>
+              <h4>${e.titulo || 'Título indisponível'}</h4>
+              <div class="info">
+                  <div class="categoria">${e.descricao || 'Descrição indisponível'}</div>
+                  <div class="valor">Valor da cota: R$ ${e.valor_cota || '0.00'}</div>
+              </div>
+              <a href="#" class="button">Ver</a>
+          `;
+
+      // Adicionar o card ao contêiner
+      eventosContainer.appendChild(card);
+    });
+
+  } catch (error) {
+    console.error("Erro ao conectar ao servidor:", error);
+    alert("Evento não existe ou indisponivel");
+  }
+}
+
 async function todos() {
   const requestOptions = {
     method: "GET",
@@ -44,7 +103,8 @@ async function todos() {
 
   } catch (error) {
     console.error("Erro ao conectar ao servidor:", error);
-    alert("Erro ao conectar ao servidor. Por favor, tente novamente mais tarde.");
+   
+    alert("Evento não existe ou indisponivel");
   }
 }
 
@@ -94,7 +154,8 @@ async function topBets() {
 
   } catch (error) {
     console.error("Erro ao conectar ao servidor:", error);
-    alert("Erro ao conectar ao servidor. Por favor, tente novamente mais tarde.");
+   
+    alert("Evento não existe ou indisponivel");
   }
 }
 
@@ -197,4 +258,3 @@ nextButton3.addEventListener('click', () => {
 function updateCarousel3() {
   carousel3.style.transform = `translateX(${scrollPosition3}px)`;
 }
-
