65ac0d16d59da128c0a6b9b9412df0811ca22203
batista29 <natabatista2908@gmail.com>
2024-11-28 15:22:50 -0300

    Login e principal funcionando

--- frontend/categorias/principal.js
+++ frontend/categorias/principal.js
@@ -1,25 +1,155 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const sidebar = document.querySelector('.sidebar');
-const closeSidebar = document.querySelector('.close-sidebar');
+async function pesquisar() {
+  const inpPesquisa = document.getElementById('inpPesquisa').value;
+  console.log(inpPesquisa);
+  const myHeaders = new Headers();
+  myHeaders.append("keyword", `${inpPesquisa}`);
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    redirect: "follow"
+  };
+  try {
+    const response = await fetch("http://127.0.0.1:3000/searchEvents", requestOptions);
+    console.log(inpPesquisa);
+    let message = await response.text();
+    message = JSON.parse(message);
+    const eventosContainer = document.querySelector('.carousel'); // A div onde os cards ficarão
+    // Limpar conteúdos antigos, se necessário
+    eventosContainer.innerHTML = '';
+    message.forEach(e => {
+      const data = new Date(e.inicioApostas);
+      const dia = data.getDate().toString().padStart(2, '0');
+      const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+      const ano = data.getFullYear();
+      const dataBR = `${dia}/${mes}/${ano}`;
+      const card = document.createElement('div');
+      card.className = 'card';
+      // Adicionar os dados ao card
+      card.innerHTML = `
+              <span class="data">${dataBR || 'Data indisponível'}</span>
+              <h4>${e.titulo || 'Título indisponível'}</h4>
+              <div class="info">
+                  <div class="categoria">${e.descricao || 'Descrição indisponível'}</div>
+                  <div class="valor">Valor da cota: R$ ${e.valor_cota || '0.00'}</div>
+              </div>
+              <a href="#" class="button">Ver</a>
+          `;
+      // Adicionar o card ao contêiner
+      eventosContainer.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Erro ao conectar ao servidor:", error);
+    alert("Evento não existe ou indisponivel");
+  }
+}
+async function todos() {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+  };
 
-// Mostrar o menu
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.add('active');
-});
+  try {
+    const response = await fetch("http://127.0.0.1:3000/getAllEvents", requestOptions);
 
-// Fechar o menu
-closeSidebar.addEventListener('click', () => {
-    sidebar.classList.remove('active');
-});
+    let message = await response.text();
+    message = JSON.parse(message);
 
-// Fechar o menu clicando fora
-window.addEventListener('click', (event) => {
-    if (!sidebar.contains(event.target) && !menuToggle.contains(event.target)) {
-        sidebar.classList.remove('active');
-    }
-});
+    const eventosContainer = document.querySelector('.carousel'); // A div onde os cards ficarão
 
+    // Limpar conteúdos antigos, se necessário
+    eventosContainer.innerHTML = '';
 
+    message.forEach(e => {
+      const data = new Date(e.inicioApostas);
+
+      const dia = data.getDate().toString().padStart(2, '0');
+      const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+      const ano = data.getFullYear();
+
+      const dataBR = `${dia}/${mes}/${ano}`;
+
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      // Adicionar os dados ao card
+      card.innerHTML = `
+              <span class="data">${dataBR || 'Data indisponível'}</span>
+              <h4>${e.titulo || 'Título indisponível'}</h4>
+              <div class="info">
+                  <div class="categoria">${e.descricao || 'Descrição indisponível'}</div>
+                  <div class="valor">Valor da cota: R$ ${e.valor_cota || '0.00'}</div>
+              </div>
+              <a href="#" class="button">Ver</a>
+          `;
+
+      // Adicionar o card ao contêiner
+      eventosContainer.appendChild(card);
+    });
+
+  } catch (error) {
+    console.error("Erro ao conectar ao servidor:", error);
+    alert("Erro ao conectar ao servidor. Por favor, tente novamente mais tarde.");
+   
+    alert("Evento não existe ou indisponivel");
+  }
+}
+
+async function topBets() {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+  };
+
+  try {
+    const response = await fetch("http://127.0.0.1:3000/topEvents", requestOptions);
+
+    let message = await response.text();
+    message = JSON.parse(message);
+
+    const eventosContainer = document.querySelector('.carousel2'); // A div onde os cards ficarão
+
+    // Limpar conteúdos antigos, se necessário
+    eventosContainer.innerHTML = '';
+
+    message.forEach(e => {
+      const data = new Date(e.inicioApostas);
+
+      const dia = data.getDate().toString().padStart(2, '0');
+      const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+      const ano = data.getFullYear();
+
+      const dataBR = `${dia}/${mes}/${ano}`;
+
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      // Adicionar os dados ao card
+      card.innerHTML = `
+              <span class="data">${dataBR || 'Data indisponível'}</span>
+              <h4>${e.titulo || 'Título indisponível'}</h4>
+              <div class="info">
+                  <div class="categoria">${e.descricao || 'Descrição indisponível'}</div>
+                  <div class="valor">Valor da cota: R$ ${e.valor_cota || '0.00'}</div>
+              </div>
+              <a href="#" class="button">Ver</a>
+          `;
+
+      // Adicionar o card ao contêiner
+      eventosContainer.appendChild(card);
+    });
+
+  } catch (error) {
+    console.error("Erro ao conectar ao servidor:", error);
+    alert("Erro ao conectar ao servidor. Por favor, tente novamente mais tarde.");
+   
+    alert("Evento não existe ou indisponivel");
+  }
+}
+
+async function carregar() {
+  todos();
+  topBets();
+}
 
 carousel = document.querySelector('.carousel');
 const prevButton = document.querySelector('.carousel-button.prev');
@@ -115,6 +245,3 @@ nextButton3.addEventListener('click', () => {
 function updateCarousel3() {
   carousel3.style.transform = `translateX(${scrollPosition3}px)`;
 }
-
-
-
